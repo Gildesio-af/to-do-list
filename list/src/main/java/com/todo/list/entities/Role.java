0ed05ec15eba9570450e78cfaebca0f575a2e997
Gildesio Araujo <gildesioaf@alu.ufc.br>
2024-10-21 12:42:38 -0300

    create relationship role user

--- list/src/main/java/com/todo/list/entities/Role.java
+++ list/src/main/java/com/todo/list/entities/Role.java
@@ -1,2 +1,27 @@
-package com.todo.list.entities;public class Role {
+package com.todo.list.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
 }
--- list/src/main/java/com/todo/list/entities/User.java
+++ list/src/main/java/com/todo/list/entities/User.java
@@ -1,2 +1,36 @@
-package com.todo.list.entities;public class User {
+package com.todo.list.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
 }
